00d29127ba228aa8f7058940c091dd471af9b98f
samudraam <112435495+samudraam@users.noreply.github.com>
2025-04-07 20:39:21 -0500

    plz2688

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        projects: resolve(__dirname, "projects.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
 });
